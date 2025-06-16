20da8789836614a024593414fe492e8434697500
salimfenzar <s.fenzar@student.avans.nl>
2025-06-16 15:12:57 +0200

    navbar

--- apps/the-circle-frontend/src/app/app.component.ts
+++ apps/the-circle-frontend/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 import { Router, RouterModule } from '@angular/router';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
     standalone: true,
-    imports: [RouterModule],
+    imports: [RouterModule, NavbarComponent],
     selector: 'avans-nx-workshop-root',
     templateUrl: './app.component.html'
 })
