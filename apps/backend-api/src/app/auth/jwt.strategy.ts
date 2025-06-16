2e9e0ae395364ad549ef4f822e58f2375e7bc069
salimfenzar <s.fenzar@student.avans.nl>
2025-06-16 14:09:12 +0200

    added authorization

---
+++ apps/backend-api/src/app/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+// apps/backend-api/src/app/auth/jwt.strategy.ts
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'truyou-secret-key',
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email, role: payload.role };
+  }
+}
