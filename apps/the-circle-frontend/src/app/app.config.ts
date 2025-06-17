b91cb85d4a0973621250900856f29e98034d734b
YassineVos <yessinboukrach@hotmail.com>
2025-06-17 14:35:07 +0200

    feat(frontend): voeg pagina toe voor actieve streamers met live-indicator en datum in NL-formaat

--- apps/the-circle-frontend/src/app/app.config.ts
+++ apps/the-circle-frontend/src/app/app.config.ts
@@ -1,10 +1,18 @@
 import { ApplicationConfig } from '@angular/core';
 import {
-  provideRouter,
-  withEnabledBlockingInitialNavigation,
+    provideRouter,
+    withEnabledBlockingInitialNavigation
 } from '@angular/router';
 import { appRoutes } from './app.routes';
+import { registerLocaleData } from '@angular/common';
+import localeNl from '@angular/common/locales/nl';
+import { LOCALE_ID } from '@angular/core';
+
+registerLocaleData(localeNl);
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+    providers: [
+        provideRouter(appRoutes, withEnabledBlockingInitialNavigation()),
+        { provide: LOCALE_ID, useValue: 'nl-NL' }
+    ]
 };
