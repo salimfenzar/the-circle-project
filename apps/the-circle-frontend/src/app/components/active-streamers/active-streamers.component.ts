884ebffae9af3a1640034a0efc7e579f2118ce6a
ChyravStrien <c.vanstrien2@student.avans.nl>
2025-06-20 15:14:37 +0200

    join stream

--- apps/the-circle-frontend/src/app/components/active-streamers/active-streamers.component.ts
+++ apps/the-circle-frontend/src/app/components/active-streamers/active-streamers.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { StreamService } from '../dashboard/streaming/streaming.service';
 
 @Component({
     selector: 'avans-nx-workshop-app-active-streamers',
@@ -13,7 +14,7 @@ import { Router } from '@angular/router';
 export class ActiveStreamersComponent implements OnInit {
     activeStreams: any[] = [];
 
-    constructor(private http: HttpClient, private router: Router) {}
+    constructor(private http: HttpClient, private router: Router, private streamService: StreamService) {}
 
     ngOnInit(): void {
         this.http.get<any[]>('http://localhost:3000/streams/active').subscribe({
@@ -29,5 +30,11 @@ export class ActiveStreamersComponent implements OnInit {
     joinStream(streamId: string) {
         // Redirect to the stream viewing page, passing the stream ID
         this.router.navigate(['/watch', streamId]);
+        this.streamService.joinStream(streamId).subscribe({
+            next: (stream) => {
+                console.log('Joined stream:', stream);
+            }
+        });
+
     }
 }
