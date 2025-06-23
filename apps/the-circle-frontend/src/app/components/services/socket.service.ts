cd15a10eba6dec8e38b4fb656cb8f38ace616dfc
YassineVos <yessinboukrach@hotmail.com>
2025-06-23 21:33:26 +0200

    Revert "Livechat messages worden succesvol opgeslagen in de database"
    This reverts commit ab4b95bd94a4d2cdcfa947d39a36a62af06e8b8f.

--- apps/the-circle-frontend/src/app/components/services/socket.service.ts
+++ apps/the-circle-frontend/src/app/components/services/socket.service.ts
@@ -4,20 +4,14 @@ import { io, Socket } from 'socket.io-client';
 
 @Injectable({ providedIn: 'root' })
 export class SocketService {
-    private socket: Socket;
+  private socket: Socket;
 
-    constructor() {
-        console.log('📡 Connecting to socket server...');
+  constructor() {
+    console.log('📡 Connecting to socket server...');
+    this.socket = io(`http://${window.location.hostname}:3000`);
+  }
 
-        const token = localStorage.getItem('access_token');
-        this.socket = io(`http://${window.location.hostname}:3000`, {
-            auth: {
-                token: token
-            }
-        });
-    }
-
-    getSocket(): Socket {
-        return this.socket;
-    }
+  getSocket(): Socket {
+    return this.socket;
+  }
 }
